c304c43cda92c05e09a9374e885df20749ca7515
soumyadghosh <soumyadeepghosh2004@zohomail.in>
2023-11-12 17:55:55 +0530

    cleanups

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: no_leading_underscores_for_local_identifiers
 
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sorted/constants.dart';
@@ -44,181 +42,189 @@ class _HomePageState extends State<HomePage> {
     }
 
     return Scaffold(
-      appBar: const YaruWindowTitleBar(),
+      appBar: const YaruWindowTitleBar(
+        title: Text('Sorted'),
+      ),
       body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 40,
-                right: 40,
-                top: 40,
-                bottom: 20,
-              ),
-              child: TextField(
-                focusNode: f1,
-                autofocus: true,
-                controller: controller,
-                decoration: InputDecoration(
-                  suffixIcon: YaruIconButton(
-                    icon: const Icon(YaruIcons.edit_clear),
-                    onPressed: () {
-                      setState(() {
-                        controller.clear();
-                      });
-                    },
-                  ),
-                  hintText: 'Enter the values to sort, separated by commas',
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(25),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 40,
+                  right: 40,
+                  top: 20,
+                  bottom: 20,
+                ),
+                child: TextField(
+                  focusNode: f1,
+                  autofocus: true,
+                  controller: controller,
+                  decoration: InputDecoration(
+                    suffixIcon: YaruIconButton(
+                      icon: const Icon(YaruIcons.edit_clear),
+                      onPressed: () {
+                        setState(() {
+                          controller.clear();
+                        });
+                      },
                     ),
-                    borderSide: BorderSide(
-                      color: Theme.of(context).primaryColor,
-                    ),
+                    hintText: 'Enter the values to sort, separated by commas',
+                    filled: true,
                   ),
                 ),
               ),
-            ),
-            TextFieldTapRegion(
-              onTapOutside: (event) {
-                f1.requestFocus();
-              },
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: YaruPopupMenuButton(
-                      tooltip: 'Select the Algorithm',
-                      enableFeedback: true,
-                      child: Text(
-                        _popupMenuTitle,
-                        style: TextStyle(
-                          fontSize: fontSize,
-                        ),
+              TextFieldTapRegion(
+                onTapOutside: (event) {
+                  f1.requestFocus();
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 20,
+                        right: 20,
+                        top: 20,
+                        bottom: 40,
                       ),
-                      itemBuilder: (BuildContext context) {
-                        return List.generate(algorithms.length, (index) {
-                          return PopupMenuItem<String>(
-                            value: algorithms[index],
-                            child: Text(
-                              algorithms[index],
-                              style: TextStyle(fontSize: fontSize),
-                            ),
-                            onTap: () => selectedAlgorithm = algorithms[index],
-                          );
-                        });
-                      },
-                      onSelected: (value) {
-                        // Update the selected option and trigger a rebuild
-                        setState(() {
-                          _popupMenuTitle = value;
-                        });
-                      },
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      String t1 = controller.text;
-                      try {
-                        listNumbers =
-                            t1.split(',').map((s) => s.trim()).toList();
-                        intNumbers =
-                            listNumbers.map((s) => int.parse(s)).toList();
-                      } catch (e) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text(
-                              e.toString(),
-                              style: TextStyle(fontSize: fontSize),
-                            ),
-                            behavior: SnackBarBehavior.floating,
-                            showCloseIcon: true,
-                            width: width - 50,
+                      child: YaruPopupMenuButton(
+                        tooltip: 'Select the Algorithm',
+                        enableFeedback: true,
+                        child: Text(
+                          _popupMenuTitle,
+                          style: TextStyle(
+                            fontSize: fontSize,
                           ),
-                        );
-                      } finally {
-                        if (selectedAlgorithm == '') {
+                        ),
+                        itemBuilder: (BuildContext context) {
+                          return List.generate(algorithms.length, (index) {
+                            return PopupMenuItem<String>(
+                              value: algorithms[index],
+                              child: Text(
+                                algorithms[index],
+                                style: TextStyle(fontSize: fontSize),
+                              ),
+                              onTap: () =>
+                                  selectedAlgorithm = algorithms[index],
+                            );
+                          });
+                        },
+                        onSelected: (value) {
+                          // Update the selected option and trigger a rebuild
+                          setState(() {
+                            _popupMenuTitle = value;
+                          });
+                        },
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        String t1 = controller.text;
+                        try {
+                          listNumbers =
+                              t1.split(',').map((s) => s.trim()).toList();
+                          intNumbers =
+                              listNumbers.map((s) => int.parse(s)).toList();
+                        } catch (e) {
                           ScaffoldMessenger.of(context).showSnackBar(
                             SnackBar(
                               content: Text(
-                                'Select an Algorithm',
+                                e.toString(),
                                 style: TextStyle(fontSize: fontSize),
                               ),
+                              behavior: SnackBarBehavior.floating,
                               showCloseIcon: true,
-                              clipBehavior: Clip.antiAliasWithSaveLayer,
+                              width: width - 50,
                             ),
                           );
+                        } finally {
+                          if (selectedAlgorithm == '') {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                  'Select an Algorithm',
+                                  style: TextStyle(fontSize: fontSize),
+                                ),
+                                showCloseIcon: true,
+                                clipBehavior: Clip.antiAliasWithSaveLayer,
+                              ),
+                            );
+                          }
+                          if (selectedAlgorithm == 'Insertion') {
+                            Stopwatch stopwatch = Stopwatch()..start();
+                            InsertionSort insertionSort =
+                                InsertionSort(intNumbers);
+                            setState(() {
+                              result = insertionSort.sort().toString();
+                              time = stopwatch.elapsed;
+                            });
+                            stopwatch.stop();
+                          }
+                          if (selectedAlgorithm == 'Bubble') {
+                            Stopwatch stopwatch = Stopwatch()..start();
+                            BubbleSort bubbleSort = BubbleSort(intNumbers);
+                            setState(() {
+                              result = bubbleSort.sort().toString();
+                              time = stopwatch.elapsed;
+                            });
+                            stopwatch.stop();
+                          }
+                          if (selectedAlgorithm == 'Merge') {
+                            Stopwatch stopwatch = Stopwatch()..start();
+                            MergeSort mergeSort = MergeSort(intNumbers);
+                            setState(() {
+                              result = mergeSort.sort().toString();
+                              time = stopwatch.elapsed;
+                            });
+                            stopwatch.stop();
+                          }
                         }
-                        if (selectedAlgorithm == 'Insertion') {
-                          Stopwatch stopwatch = Stopwatch()..start();
-                          InsertionSort insertionSort =
-                              InsertionSort(intNumbers);
-                          setState(() {
-                            result = insertionSort.sort().toString();
-                            time = stopwatch.elapsed;
-                          });
-                          stopwatch.stop();
-                        }
-                        if (selectedAlgorithm == 'Bubble') {
-                          Stopwatch stopwatch = Stopwatch()..start();
-                          BubbleSort bubbleSort = BubbleSort(intNumbers);
-                          setState(() {
-                            result = bubbleSort.sort().toString();
-                            time = stopwatch.elapsed;
-                          });
-                          stopwatch.stop();
-                        }
-                        if (selectedAlgorithm == 'Merge') {
-                          Stopwatch stopwatch = Stopwatch()..start();
-                          MergeSort mergeSort = MergeSort(intNumbers);
-                          setState(() {
-                            result = mergeSort.sort().toString();
-                            time = stopwatch.elapsed;
-                          });
-                          stopwatch.stop();
-                        }
-                      }
-                    },
-                    style: ButtonStyle(
+                      },
+                      style: ButtonStyle(
                         backgroundColor: MaterialStatePropertyAll(
                             Theme.of(context).primaryColor),
                         shape: const MaterialStatePropertyAll<
                             RoundedRectangleBorder>(
                           RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(25)),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(40),
+                            ),
                           ),
-                        )),
-                    child: Text(
-                      'Calculate',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: fontSize,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: SizedBox(
-                      height: height - 300,
-                      width: width - 50,
-                      child: YaruBanner(
-                        child: Text(
-                          '$result\n\nTime Taken: $time \n\n$width $height',
-                          style: TextStyle(fontSize: fontSize),
                         ),
-                        onTap: () async {
-                          await Clipboard.setData(
-                              ClipboardData(text: result.toString()));
-                        },
+                      ),
+                      child: Text(
+                        'Calculate',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: fontSize,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: SizedBox(
+                        height: height - 350,
+                        width: width - 50,
+                        child: YaruBanner(
+                          child: Text(
+                            '$result\n\nTime Taken: $time',
+                            style: TextStyle(fontSize: fontSize),
+                          ),
+                          onTap: () async {
+                            await Clipboard.setData(
+                                ClipboardData(text: result.toString()));
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
