3a005728681c344ef3ab11effad4c48ca87ae39c
soumyadghosh <soumyadeepghosh2004@zohomail.in>
2023-11-20 15:38:42 +0530

    Add more details in about

--- lib/common/widgets/about_section.dart
+++ lib/common/widgets/about_section.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sorted/constants.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:yaru/yaru.dart';
 import 'package:yaru_icons/yaru_icons.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 
@@ -23,24 +24,27 @@ class _AboutSectionState extends State<AboutSection> {
         ),
         onPressed: () {
           showAboutDialog(
-              context: context,
-              applicationName: 'Sorted',
-              applicationVersion: '0.1',
-              applicationIcon: YaruIconButton(
-                icon: Image.asset(appIcon),
-                onPressed: () => launchUrl(Uri.parse(githubProject)),
-              ),
-              children: [
-                InkWell(
-                  child: Text(
-                    'Copyright © Soumyadeep Ghosh 2023 and onwards. All rights reserved.',
-                    style: TextStyle(color: Theme.of(context).primaryColor),
+            context: context,
+            applicationName: 'Sorted',
+            applicationVersion: '0.1',
+            applicationIcon: YaruIconButton(
+              icon: Image.asset(appIcon),
+              onPressed: () => launchUrl(Uri.parse(githubProject)),
+            ),
+            children: List.generate(
+              contributors.length,
+              (index) {
+                final c = contributors.entries.elementAt(index);
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                    child: Text(c.key),
+                    onTap: () => launchUrl(Uri.parse(c.value)),
                   ),
-                  onTap: () => launchUrl(
-                    Uri.parse(githubMe),
-                  ),
-                )
-              ]);
+                );
+              },
+            ),
+          );
         },
       ),
     );
