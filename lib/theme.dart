293be6b9fc60c7708a4fa9e030bfab0fa9780a5b
soumyadghosh <soumyadeepghosh2004@zohomail.in>
2023-11-22 02:31:16 +0530

    custom theming for mobile

---
+++ lib/theme.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+ThemeData m3Theme({
+  Brightness brightness = Brightness.light,
+  Color color = Colors.greenAccent,
+  Color? scaffoldBackgroundColor,
+  Color? foregroundColor,
+}) {
+  final dividerColor = brightness == Brightness.light
+      ? const Color.fromARGB(48, 0, 0, 0)
+      : const Color.fromARGB(18, 255, 255, 255);
+  return ThemeData(
+    useMaterial3: true,
+    dividerColor: dividerColor,
+    dividerTheme: DividerThemeData(
+      color: dividerColor,
+      space: 1.0,
+      thickness: 0.0,
+    ),
+    colorScheme: ColorScheme.fromSeed(
+      surfaceTint: Colors.transparent,
+      seedColor: color,
+      brightness: brightness,
+    ),
+    splashFactory:
+        Platform.isMacOS || Platform.isIOS ? NoSplash.splashFactory : null,
+    scaffoldBackgroundColor: scaffoldBackgroundColor ??
+        (brightness == Brightness.light ? Colors.white : Colors.black),
+    appBarTheme: AppBarTheme(
+      foregroundColor: foregroundColor ??
+          (brightness == Brightness.light ? Colors.white : Colors.black),
+    ),
+  );
+}
